ec36ed9a0580eff1469c2c571a11ace1b855a4a4
abhinavdayal <abhinav.dayal@hotmail.com>
2020-04-12 03:12:25 +0530

    removed F

--- EVA4/eva4models/s11net.py
+++ EVA4/eva4models/s11net.py
@@ -14,12 +14,14 @@ class ResBlock(nn.Module):
         super(ResBlock, self).__init__()
         self.conv1 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
+        self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
+        self.relu2 = nn.ReLU()
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.relu1(self.bn1(self.conv1(x)))
+        out = self.relu1(self.bn2(self.conv2(out)))
         out = torch.add(x, out)
         return out
 
@@ -28,12 +30,14 @@ class S11Block(nn.Module):
         super(S11Block, self).__init__()
         self.parallel = parallel
         self.conv = nn.Conv2d(in_planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
+        self.maxpool = nn.MaxPool2d(2)
         self.bn = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU()
         if parallel:
             self.res = ResBlock(planes)
 
     def forward(self, x):
-        out = F.relu(self.bn(F.max_pool2d(self.conv(x), 2)))
+        out = self.relu(self.bn(self.maxpool(self.conv(x))))
         if self.parallel:
             out = self.res(out)
         return out
@@ -48,6 +52,8 @@ class S11Net(Net):
     self.layer2 = S11Block(128, 256, False)
     self.layer3 = S11Block(256, 512)
 
+    #ending layer or layer-4
+    self.maxpool = nn.MaxPool2d(4)
     self.fc = self.create_conv2d(512, 10, kernel_size=(1,1), padding=0, bn=False, relu=False)
 
 
@@ -56,7 +62,7 @@ class S11Net(Net):
     x = self.layer1(x)
     x = self.layer2(x)
     x = self.layer3(x)
-    x = F.max_pool2d(x, 4)
+    x = self.maxpool(x)
     x = self.fc(x)
     x = x.view(-1,10)
     return F.log_softmax(x,dim=-1)
