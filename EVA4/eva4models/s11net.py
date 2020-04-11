351bcfac2275bbf339522dadba3d837f86d0555e
abhinavdayal <abhinav.dayal@hotmail.com>
2020-04-12 03:43:58 +0530

    res

--- EVA4/eva4models/s11net.py
+++ EVA4/eva4models/s11net.py
@@ -68,10 +68,55 @@ class S11Net(Net):
     return F.log_softmax(x,dim=-1)
 
 
+from eva4modeltrainer import ModelTrainer
 
+class Net2(nn.Module):
+    """
+    Base network that defines helper functions, summary and mapping to device
+    """
+    def conv2d(self, in_channels, out_channels, kernel_size=(3,3), dilation=1, groups=1, padding=1, bias=False, padding_mode="zeros"):
+      return [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, groups=groups, dilation=dilation, padding=padding, bias=bias, padding_mode=padding_mode)]
+
+    def separable_conv2d(self, in_channels, out_channels, kernel_size=(3,3), dilation=1, padding=1, bias=False, padding_mode="zeros"):
+      return [nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, groups=in_channels, dilation=dilation, padding=padding, bias=bias, padding_mode=padding_mode),
+              nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1,1), bias=bias)]
+
+    def activate(self, l, out_channels, bn=True, dropout=0, relu=True,max_pooling=0):
+      if(max_pooling>0):
+        l.append(nn.MaxPool2d(2,2))
+      if bn:
+        l.append(nn.BatchNorm2d(out_channels))
+      if dropout>0:
+        l.append(nn.Dropout(dropout))
+      if relu:
+        l.append(nn.ReLU())
+
+      return nn.Sequential(*l)
+
+    def create_conv2d(self, in_channels, out_channels, kernel_size=(3,3), dilation=1, groups=1, padding=1, bias=False, bn=True, dropout=0, relu=True, padding_mode="zeros",max_pooling=0):
+      return self.activate(self.conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, groups=groups, dilation=dilation, padding=padding, bias=bias, padding_mode=padding_mode), out_channels, bn, dropout, relu,max_pooling)
+
+    def create_depthwise_conv2d(self, in_channels, out_channels, kernel_size=(3,3), dilation=1, padding=1, bias=False, bn=True, dropout=0, relu=True, padding_mode="zeros"):
+      return self.activate(self.separable_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, dilation=dilation, padding=padding, bias=bias, padding_mode=padding_mode),
+                 out_channels, bn, dropout, relu)
+
+    def __init__(self, name="Model"):
+        super(Net, self).__init__()
+        self.trainer = None
+        self.name = name
+
+    def summary(self, input_size): #input_size=(1, 28, 28)
+      summary(self, input_size=input_size)
+
+    def gotrain(self, optimizer, train_loader, test_loader, epochs, statspath, scheduler=None, batch_scheduler=False, L1lambda=0):
+      self.trainer = ModelTrainer(self, optimizer, train_loader, test_loader, statspath, scheduler, batch_scheduler, L1lambda)
+      self.trainer.run(epochs)
+
+    def stats(self):
+      return self.trainer.stats if self.trainer else None
 
 #implementation of the new resnet model
-class newResnetS11(Net):
+class newResnetS11(Net2):
   def __init__(self,name="Model",dropout_value=0):
     super(newResnetS11,self).__init__(name)
     self.prepLayer=self.create_conv2d(3, 64, dropout=dropout_value)
